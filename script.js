31665d6ae3385b3554b9097cb6d0081efe423b23
mattsedlar <sedlarm@gmail.com>
2015-05-11 15:10:48 -0700

    Social functions added, space between share icons

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 var marks = [
 
   {
-    name: "Gov. Jerry Brown",
+    name: "Jerry Brown",
+    title: "Governor",
     email: "",
     twitter: "@JerryBrownGov"
   },
 
   {
     name: "Kevin de León",
+    title: "California Senate President pro Tempore",
     email: "",
     twitter: "@kdeleon"
   },
 
   {
     name: "Toni Atkins",
+    title: "California Speaker of the Assembly",
     email: "",
     twitter: "@toniatkins"
   }
@@ -38,5 +41,35 @@ function shuffleArray(array) {
 $(document).ready( function() {
 
 shuffleArray(marks);
+
+$("#facebook").click( function() {
+    FB.ui({
+        method: 'feed',
+        link: 'http://example.com',
+        name: "Tell " + marks[0].title + " " + marks[0].name + ": Let's fix Medi-Cal",
+        caption: 'Learn more about how we can get Medi-Cal fully funded for patients and care providers.'
+        }, function(response){});
+});
+
+$("#twitter").click( function() {
+  var twitterUrl = "http://twitter.com/share/?",
+  text = "text=We need to fix Medi-Cal " + marks[0].twitter,
+  quizUrl = "&url=http://example.com";
+
+  var width  = 575,
+  height = 400,
+  left   = ($(window).width()  - width)  / 2,
+  top    = ($(window).height() - height) / 2,
+  url    = twitterUrl + text + quizUrl,
+  opts   = 'status=1' +
+           ',width='  + width  +
+           ',height=' + height +
+           ',top='    + top    +
+           ',left='   + left;
+
+  window.open(url, 'twitter', opts);
+
+});
+
 
 });
