9795ec65de7b2c48d74f323f1cb91cbb0da30ce7
mattsedlar <sedlarm@gmail.com>
2015-05-11 21:18:38 -0700

    Created parts objects, zip code to form, social share functionality

--- script.js
+++ script.js
@@ -23,6 +23,24 @@ var marks = [
 
 ];
 
+var parts = {
+
+  brain: {
+    twitter: ": When having a stroke, you shouldn't need to worry if your doc takes Medi-Cal. [link to pic]  #AB366 #WeCare4CA #CADem15",
+    facebook: "After Luz was rushed to the Emergency Room with a stroke, she discovered the surgeon wouldn’t accept her Medi-Cal coverage, leaving her $25,000 in debt. Read Luz’s story, and take action to fully fund Medi-Cal for California.",
+    image: "",
+    email: ""
+  },
+
+  heart: {
+    twitter: "Ricardo has a heart condition, but 5 doctors turned him away b/c he is insured by Medi-Cal. [link to pic] #SB243 #WeCare4CA #CADem15",
+    facebook: "Ricardo was born with a heart murmur, but because he is insured by Medi-Cal, he can’t get the regular check ups he needs to stay healthy. Read his story and take action to fully fund Medi-Cal for California.",
+    image: "",
+    email: ""
+  }
+
+};
+
 /**
  * Randomize array element order in-place.
  * Using Fisher-Yates shuffle algorithm.
@@ -43,24 +61,23 @@ $(document).ready( function() {
 shuffleArray(marks);
 
 $("#facebook").click( function() {
-    FB.ui({
-        method: 'feed',
-        link: 'http://example.com',
-        name: "Tell " + marks[0].title + " " + marks[0].name + ": Let's fix Medi-Cal",
-        caption: 'Learn more about how we can get Medi-Cal fully funded for patients and care providers.'
-        }, function(response){});
+      FB.ui({
+          method: 'feed',
+          link: 'http://mattsedlar.net/development/social-media-app/share.php',
+          caption: parts[part].facebook
+          }, function(response){});
 });
 
 $("#twitter").click( function() {
   var twitterUrl = "http://twitter.com/share/?",
-  text = "text=We need to fix Medi-Cal " + marks[0].twitter,
-  quizUrl = "&url=http://example.com";
+  text = "text=" + marks[0].twitter + " " + parts[part].twitter,
+  picUrl = "&url=" + parts[part].image;
 
   var width  = 575,
   height = 400,
   left   = ($(window).width()  - width)  / 2,
   top    = ($(window).height() - height) / 2,
-  url    = twitterUrl + text + quizUrl,
+  url    = twitterUrl + text + picUrl,
   opts   = 'status=1' +
            ',width='  + width  +
            ',height=' + height +
