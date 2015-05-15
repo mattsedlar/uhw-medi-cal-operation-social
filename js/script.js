1f05a6e5f8d8f1b817c1a7825520fbf64ed2f7e3
mattsedlar <sedlarm@gmail.com>
2015-05-14 17:02:49 -0700

    More changes from client

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ var marks = [
 var parts = {
 
   brain: {
-    name: "Luz Maria Arellano, 65, San Diego",
+    name: "Luz, age 65, San Diego",
     twitter: "." + marks[0].twitter + " " + marks[1].twitter + ": having a stroke, you shouldn't need to worry if Medi-Cal will cover your treatment. ",
     facebook: "After Luz was rushed to the ER with a stroke and operated on, she learned her surgeon didn't accept Medi-Cal. Join the campaign and take action to fully fund Medi-Cal for California.",
     image: "images/luz-a.jpg",
@@ -27,23 +27,23 @@ var parts = {
   },
 
   heart: {
-    name: "Ricardo Ponce, 22, Fresno, Laid Off - Electronics Recycling Worker",
-    twitter: "Ricardo has a heart condition, but b/c he has Medi-Cal he couldn't get his medication. " + marks[0].twitter + " " + marks[1].twitter,
+    name: "Ricardo, age 22, Fresno, Laid Off - Electronics Recycling Worker",
+    twitter: "Ricardo has a heart condition, but because he has Medi-Cal he couldn't get his medication. " + marks[0].twitter + " " + marks[1].twitter,
     facebook: "Ricardo was born with a heart murmur, but because he is insured by Medi-Cal, he couldn't get the treatment and medications he needs. Join the campaign and take action to fully fund Medi-Cal for California.",
     image: "images/ricardo-p.jpg",
     email: "Can you imagine knowing you have a ticking time bomb in your chest, with no way to make sure it doesn't go off?<br><br>That's what Ricardo Ponce in Fresno dealt with every day for eight months.<br><br>Ricardo was born with a heart murmur, and he saw a doctor regularly for treatment and medication. But when he turned 21 and became a Medi-Cal beneficiary, all that ended. For eight months, he could not find a primary doctor who was accepting new Medi-Cal patients, forcing his mother to pay out of pocket for his treatment and medications.<br><br>Medi-Cal's low payment rates to healthcare providers leave patients like Ricardo with nowhere to turn for care, and at risk of serious health problems.<br><br>Please support fully funding Medi-Cal, and help people like Ricardo get the care they need.<br><br>Sincerely,<br><br>"
   },
 
   knee: {
-    name: "Lourdes Ortiz, 45, Compton",
-    twitter: "In an accident you shouldn't need to travel further for an ER that takes Medi-Cal " + marks[0].twitter + " " + marks[1].twitter,
+    name: "Lourdes, age 45, Compton",
+    twitter: "In an accident, you shouldn't need to travel further for an ER that takes Medi-Cal " + marks[0].twitter + " " + marks[1].twitter,
     facebook: "After a car accident, Lourdes was turned away from the closest hospital, because they couldn't accept her Medi-Cal coverage. Join the campaign and take action to fully fund Medi-Cal",
     image: "images/lourdes.jpg",
-    email: "After a car accident, the last thing you should have to worry about is whether the nearest emergency room can give you the treatment you need. But Lourdes Ortiz from Compton found herself in just that situation.<br><br>Lourdes was rushed to the closest hospital, but the hospital couldn't take her because they weren't able to accept Medi-Cal patients. But even after she got care at another hospital, Lourdes' troubles weren't over.<br><br>The accident tore a ligament in her knee, and Lourdes has been waiting almost six months to see a specialist and get the care that will help get her back on her feet. The wait hasn't just meant daily pain for Lourdes – it's meant losing her job.<br><br>Medi-Cal's low payment rates to healthcare providers mean fewer doctors and hospitals are able to provide treatment to Medi-Cal patients, and that means delays in care that can cause serious complications.<br><br>Please support fully funding Medi-Cal, and help people like Lourdes get the care they need.<br><br>Sincerely,<br><br>"
+    email: "After a car accident, the last thing you should have to worry about is whether the nearest emergency room can give you the treatment you need. But Lourdes Ortiz from Compton found herself in just that situation.<br><br>Lourdes was rushed to the closest hospital, but the hospital couldn't take her because they weren't able to accept Medi-Cal patients. And even after she got care at another hospital, Lourdes' troubles weren't over.<br><br>The accident tore a ligament in her knee, and Lourdes has been waiting almost six months to see a specialist and get the care that will help get her back on her feet. The wait hasn't just meant daily pain for Lourdes – it's meant losing her job.<br><br>Medi-Cal's low payment rates to healthcare providers mean fewer doctors and hospitals are able to provide treatment to Medi-Cal patients, and that means delays in care that can cause serious complications.<br><br>Please support fully funding Medi-Cal, and help people like Lourdes get the care they need.<br><br>Sincerely,<br><br>"
   },
 
   stomach: {
-    name: "Jose Berumen, 54, Oakland, Handyman",
+    name: "Jose, age 54, Oakland, Handyman",
     twitter: "No one should have a scheduled surgery cancelled just because their insurance is Medi-Cal " + marks[0].twitter + " " + marks[1].twitter,
     facebook: "Jose has been turned away from his scheduled hernia surgery twice because he's covered by Medi-Cal. Join the campaign and take action to fully fund Medi-Cal for California.",
     image: "images/jose-b.jpg",
@@ -51,23 +51,23 @@ var parts = {
   },
 
   lungs: {
-    name: "Kristi Johnson, 32, Fresno, Law Student & Law Clerk",
-    twitter: "No one should have to rely on expired asthma medicine b/c they're covered by Medi-Cal. " + marks[0].twitter + " " + marks[1].twitter,
+    name: "Kristi, age 32, Fresno, Law Student & Law Clerk",
+    twitter: "No one should have to rely on expired asthma medicine because they're covered by Medi-Cal. " + marks[0].twitter + " " + marks[1].twitter,
     facebook: "Imagine being unable to breathe because you can't use your Medi-Cal coverage to see a doctor for three months. Join the campaign and take action to fully fund Medi-Cal for California.",
     image: "images/kristi-j.jpg",
     email: "Would you take expired medication? What if your only other choice was no medication at all?<br><br>Kristi Johnson in Fresno had to face that choice for nearly six months.<br><br>Kristi is a law student with asthma, and she is covered by Medi-Cal. When she called to make an appointment with her doctor, Kristi discovered the doctor did not accept Medi-Cal patients. It took nearly three months for Kristi just to get an appointment with another doctor to renew her asthma prescriptions, and in the meantime she relied on expired medication to treat her condition.<br><br>Doctors like Kristi's can't afford to take Medi-Cal patients because of California's low reimbursement rates. That means patients with chronic conditions like Kristi find themselves unable to get their medication.<br><br>Please support fully funding Medi-Cal, and help people like Kristi to get the care they need.<br><br>Sincerely,<br><br>"
   },
 
   foot: {
-    name: "Tioy Mulvil, 19, City of Ione",
-    twitter: "When Medi-Cal patients like Tioy have to wait for care, it means higher costs %26 worse health " + marks[0].twitter + " " + marks[1].twitter,
+    name: "Tioy, age 19, City of Ione",
+    twitter: "When Medi-Cal patients like Tioy have to wait for care, it means higher costs %26 worse health. " + marks[0].twitter + " " + marks[1].twitter,
     facebook: "When does an ingrown toenail turn into emergency surgery? When Medi-Cal patients like Tioy can't get timely care. Join the campaign and take action to fully fund Medi-Cal for California.",
     image: "images/tioy.jpg",
     email: "When does a simple ingrown toenail turn into emergency surgery? When Medi-Cal patients are denied timely care.<br><br>Tioy Mulvil from the city of Ione, California is a Medi-Cal patient with autism.<br><br>When Tioy's mother noticed his ingrown toenail, she worked to get him treatment right away – but he had to wait three months for what should have been a routine procedure. While they were waiting for the appointment, the nail became infected and Tioy had to have emergency surgery on his foot.<br><br>When Medi-Cal patients are forced to wait for care, it means more complicated procedures that cost the state more of our limited healthcare dollars – and it means worse health and longer recovery times for patients like Tioy.<br><br>Please support fully funding Medi-Cal, and help Tioy and others like him get the care they need.<br><br>Sincerely,<br><br>"
   },
 
   leftArm: {
-    name: "Jenevy Guzman, 9, Fresno",
+    name: "Jenevy, age 9, Fresno",
     twitter: "9-year-old Jenevy went months w/o care for a dangerous allergy, b/c she's covered by Medi-Cal " + marks[0].twitter + " " + marks[1].twitter,
     facebook: "It took 9-year-old Jenevy more than three months to see a specialist who diagnosed her with a life-threatening allergy. Join the campaign and take action to fully fund Medi-Cal for California.",
     image: "images/jenevy.jpg",
@@ -75,7 +75,7 @@ var parts = {
   },
 
   rightHand: {
-    name: "Gilberto Uribe, 57, Oakland, Mariachi",
+    name: "Gilberto, age 57, Oakland, Mariachi",
     twitter: "How can musician Gilberto work when he has to wait months for an arthritis appointment? " + marks[0].twitter + " " + marks[1].twitter,
     facebook: "As a musician, Gilberto needs his hands in order to work, but because he has Medi-Cal coverage, he has to wait months for an appointment to get his arthritis medicine. Join the campaign and take action to fully fund Medi-Cal for California.",
     image: "images/gilberto.jpg",
